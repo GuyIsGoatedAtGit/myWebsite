dd46169ec4781a5e3f514a88b162bb5e75cfa9c9
GuyIsGoatedAtGit <loganthatcher12@gmail.com>
2026-05-25 09:03:13 -0600

    updated website

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.querySelector("button").onclick = () => {
-  alert("Hello!");
+document.getElementById("btn1").onclick = () => {
+  alert("First button");
+};
+
+document.getElementById("btn2").onclick = () => {
+  alert("Second button");
+};
+document.getElementById("secretBtn").onclick = () => {
+  alert("secret button");
 };
